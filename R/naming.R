d72686421c540ae975d5446d91e4b905a9734bd5
Krzysztof Joachimiak <kjoa.92@gmail.com>
2020-03-14 19:51:33 +0100

    vignette

---
+++ R/naming.R
@@ -0,0 +1,18 @@
+#' @name naming_k
+#' @title Naming convention, which adds k prefix for each key, capitalizes and removes file extension
+#' @param path full path or its element
+#' @importFrom tools file_path_sans_ext
+#' @importFrom stringi stri_trans_totitle
+#' @examples
+#' naming_k("path/to/myfile.txt")
+#' # Using with full_path_chain
+#' unlink("files", recursive = TRUE)
+#' create_sample_dir(name = "files")
+#' full.path.chain <- full_path_chain("files", naming = naming_k)
+#' full.path.chain
+#' create_sample_dir(name = "files")
+#' # Using with path_chain / create_path_chain
+#' @export
+naming_k <- function(path){
+  paste0("k", tools::file_path_sans_ext(stringi::stri_trans_totitle(basename(path))))
+}
---
+++ R/path-chain-package.R
@@ -0,0 +1,13 @@
+#' Package path.chain
+#'
+#' The aim of this package is to provide tools,
+#' which allow us to represent directory structure as
+#' nested R objects. It can be easily saved as .yaml
+#' files so that we can later load it and use in pur project.
+#'
+#' @author Krzysztof Joachimiak
+#' @keywords package
+#' @name path.chain-package
+#' @aliases path.chain
+#' @docType package
+NULL
